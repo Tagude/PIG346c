0c9d3cf08e9b3709e9fdbb644ed82ec186fc346a
Tagude <tania.agudelomurillo@gmail.com>
2026-01-26 14:57:27 -0500

    Se incluyen los parametros que vienen de la clase padre, y se muestra la información de cada clase.

--- Mision1/Proyecto1/src/com/empresa/gestion/app/Main.java
+++ Mision1/Proyecto1/src/com/empresa/gestion/app/Main.java
@@ -5,12 +5,20 @@ import com.empresa.gestion.service.NominaService;
 
 public class Main {
     public static void main(String[] args) {
-        Empleado empleado1 = new Empleado("Ana", 28, 3000.00);
-        Gerente gerente1 = new Gerente("Luis", 40, 6000.00, "Ventas");
+        Empleado empleado1 = new Empleado("Ana", 28, true, 'F', 3000.00, 160);
+        Gerente gerente1 = new Gerente("Luis", 40, true, 'M', 6000.00, 180, "Ventas", 500.0);
         empleado1.presentarse();
         gerente1.presentarse();
         NominaService nominaService = new NominaService();
+        System.out.println("====EMPLEADO====");
+        empleado1.mostrarInformacionBasica();
+        System.out.println("Salario mensual de " + empleado1.getNombre() + ": " + empleado1.calcularSalarioMensual());
         System.out.println("Nómina anual de " + empleado1.getNombre() + ": " + nominaService.calcularNomina(empleado1));
+        System.out.println("Puede trabajar? " + empleado1.getNombre() + ": " + empleado1.puedeTrabajar());
+        System.out.println("====GERENTE====");
+        gerente1.mostrarInformacionBasica();
+        System.out.println("Salario mensual de " + gerente1.getNombre() + ": " + gerente1.calcularSalarioMensual());
         System.out.println("Nomina anual de " + gerente1.getNombre() + ": " + nominaService.calcularNomina(gerente1));
+        System.out.println("Puede trabajar? " + gerente1.getNombre() + ": " + gerente1.puedeTrabajar());
     }
 }
--- Mision1/Proyecto1/src/com/empresa/gestion/model/Gerente.java
+++ Mision1/Proyecto1/src/com/empresa/gestion/model/Gerente.java
@@ -2,10 +2,12 @@ package com.empresa.gestion.model;
 
 public class Gerente extends Empleado {
     private String departamento;
+    private double bono;
 
-    public Gerente(String nombre, int edad, boolean activo, char genero, double salario, String departamento) {
-        super(nombre, edad, activo, genero, salario);
+    public Gerente(String nombre, int edad, boolean activo, char genero, double salario, int horasTrabajadas, String departamento, double bono) {
+        super(nombre, edad, activo, genero, salario, horasTrabajadas);
         this.departamento = departamento;
+        this.bono = bono;
     }
 
     public String getDepartamento() {
@@ -16,11 +18,24 @@ public class Gerente extends Empleado {
         this.departamento = departamento;
     }
 
+    public double getBono() {
+        return bono;
+    }
+
+    public void setBono(double bono) {
+        this.bono = bono;
+    }
+
     //Sobrescritura de método, polimorfismo
     @Override
     public void presentarse() {
         super.presentarse();
         System.out.println("Soy el gerente del departamento de: " + departamento);
     }
-    
+
+    @Override
+    public double calcularSalarioMensual() {
+        double salarioBase = super.calcularSalarioMensual();
+        return salarioBase + bono;
+    }
 }
