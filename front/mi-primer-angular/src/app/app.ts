bbc4d066dc8af7776a9840b85793c2b5a6369c84
Tagude <tania.agudelomurillo@gmail.com>
2026-01-26 17:35:46 -0500

    se crean los componentes y se renderiza desde ahí

--- front/mi-primer-angular/src/app/app.ts
+++ front/mi-primer-angular/src/app/app.ts
@@ -1,25 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserService } from './services/user.service/user.service';
-import { JsonPipe } from '@angular/common';
-import { ChangeDetectorRef } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, JsonPipe],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('mi-primer-angular');
-
-  users: any[] = [];
-
-  constructor(private userService: UserService, private cdRef: ChangeDetectorRef) {
-    this.userService.getUsers().subscribe(data => {
-      this.users = data;
-      this.cdRef.detectChanges(); // Asegura que la vista se actualice
-      console.log(this.users);
-    });
-  }
 }
---
+++ front/mi-primer-angular/src/app/view/user/user.component/user.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+/* Librerias */
+import { UserService } from '../../../services/user.service/user.service';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core';
+
+@Component({
+  selector: 'app-user.component',
+  imports: [JsonPipe, CommonModule],
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+})
+export class UserComponent {
+  users : any[] = [];
+
+  constructor(private userService: UserService, private cdr: ChangeDetectorRef) {
+    this.userService.getUsers().subscribe((data: any) => {
+      this.users = data;
+      console.log(this.users);
+      this.cdr.detectChanges();
+    });
+  }
+}
