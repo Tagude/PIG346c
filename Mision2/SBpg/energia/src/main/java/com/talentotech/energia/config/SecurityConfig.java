e41ea97480bb7f9dd96872b238d52c32bd51e232
Tagude <tania.agudelomurillo@gmail.com>
2026-03-06 21:12:51 -0500

    Solución al problema de compilación de Lombok

--- Mision2/SBpg/energia/src/main/java/com/talentotech/energia/config/SecurityConfig.java
+++ Mision2/SBpg/energia/src/main/java/com/talentotech/energia/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .requestMatchers("/api/users/login").permitAll()
                 .requestMatchers("/api/country").permitAll()
                 .requestMatchers("/api/region").permitAll()
-                .requestMatchers("api/company").permitAll()
+                .requestMatchers("/api/company").permitAll()
                 .requestMatchers("/api/energy-type").permitAll()
                 .requestMatchers("/api/power-plant").permitAll()
                 .requestMatchers("/api/measurement-type").permitAll()
--- Mision2/SBpg/energia/src/main/java/com/talentotech/energia/model/User.java
+++ Mision2/SBpg/energia/src/main/java/com/talentotech/energia/model/User.java
@@ -1,4 +1,5 @@
 package com.talentotech.energia.model;
+
 import java.lang.annotation.Inherited;
 import java.time.LocalDateTime;
 import javax.annotation.processing.Generated;
@@ -12,7 +13,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable =false, unique = true)
+    @Column(nullable = false, unique = true)
     private String username;
     @Column(nullable = false, unique = true)
     private String email;
@@ -22,27 +23,35 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Role role;
     @CreationTimestamp
-    @Column (name = "created_at", nullable = false, updatable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
     @UpdateTimestamp
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
-    public User(){}
-       public Long getId() {
+
+    public User() {
+    }
+
+    public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
